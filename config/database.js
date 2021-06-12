aa550baa216f00f71e048f77096a18a540bbf660
Junosoft <junosoftltd@gmail.com>
2021-06-12 17:08:03 +0200

    strapi added hotel functionality

--- config/database.js
+++ config/database.js
@@ -18,3 +18,26 @@ module.exports = ({ env }) => ({
     },
   },
 })
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'cluster0.zhnv8.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'inodashboard'),
+//         username: env('DATABASE_USERNAME', 'Admin'),
+//         password: env('DATABASE_PASSWORD', 'Juno@12345'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
+
